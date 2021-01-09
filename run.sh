c628e32b4aff5ddf4d88a6c937b596576f14c138
bfsmaksym <73019333+bfsmaksym@users.noreply.github.com>
2021-01-10 00:52:52 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ echo "Adding env variables..."
 export PATH=/root/bin:$PATH
 
 #Path to k8s config file
-KUBECONFIG=/home/maxible/baf/build/config
+KUBECONFIG=/home/baf/build/config
 
 
 echo "Running the playbook..."
-exec ansible-playbook -vv /home/maxible/baf/platforms/shared/configuration/site.yaml --inventory-file=/home/maxible/baf/platforms/shared/inventory/ -e "@/home/maxible/baf/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+exec ansible-playbook -vv /home/baf/platforms/shared/configuration/site.yaml --inventory-file=/home/baf/platforms/shared/inventory/ -e "@/home/baf/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
