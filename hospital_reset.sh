9002bc09374b6185dec9c7caa1d01365ac220f39
kawthers <63705718+kawthers@users.noreply.github.com>
2021-01-27 13:50:52 +0200

    Update hospital_reset.sh

--- hospital_reset.sh
+++ hospital_reset.sh
@@ -7,8 +7,8 @@ echo "Adding env variables..."
 export PATH=/root/bin:$PATH
 
 #Path to k8s config file
-KUBECONFIG=/home/blockchain-automation-framework/build/config_hospital
+KUBECONFIG=/home/blockchain-automation-framework/build/config
 
 
 echo "Running the playbook..."
-exec ansible-playbook -vvvv /home/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/blockchain-automation-framework/build/hospital.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
+exec ansible-playbook -v /home/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/blockchain-automation-framework/build/hospital.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
